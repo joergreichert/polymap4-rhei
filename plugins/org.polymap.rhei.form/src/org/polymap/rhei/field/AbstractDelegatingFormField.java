8a13420a772558ca688774f597587c778339f4f8
Joerg Reichert <joerg@mapzone.io>
2015-10-25 14:31:27 +0100

    #14: Create feature styler
    (https://github.com/Polymap4/polymap4-p4/issues/issue/14)
    
    common (abstract) form field to delegate actual configuration to another
    panel

---
+++ plugins/org.polymap.rhei.form/src/org/polymap/rhei/field/AbstractDelegatingFormField.java
@@ -0,0 +1,162 @@
+/*
+ * polymap.org 
+ * Copyright (C) 2015 individual contributors as indicated by the @authors tag. 
+ * All rights reserved.
+ * 
+ * This is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ */
+package org.polymap.rhei.field;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.polymap.rhei.form.IFormToolkit;
+
+/**
+ * @author Joerg Reichert <joerg@mapzone.io>
+ *
+ */
+public abstract class AbstractDelegatingFormField<T>
+        implements IFormField {
+
+    private IFormFieldSite site;
+
+    private Button         button;
+
+    private T              currentValue;
+
+    private Object         loadedValue;
+
+    private boolean        deferredEnabled = true;
+
+
+    @Override
+    public void init( IFormFieldSite site ) {
+        this.site = site;
+
+    }
+
+
+    @Override
+    public void dispose() {
+        button.dispose();
+    }
+
+
+    @Override
+    public Control createControl( Composite parent, IFormToolkit toolkit ) {
+        button = toolkit.createButton( parent, getInitialLabel(), SWT.PUSH );
+        button.addSelectionListener( new SelectionAdapter() {
+
+            public void widgetSelected( org.eclipse.swt.events.SelectionEvent e ) {
+                handleSelectionEvent( site, e );
+            }
+
+        } );
+        button.setEnabled( deferredEnabled );
+        return button;
+    }
+
+
+    protected void handleSelectionEvent( IFormFieldSite site, org.eclipse.swt.events.SelectionEvent e ) {
+        site.fireEvent( AbstractDelegatingFormField.this, IFormFieldListener.VALUE_CHANGE, currentValue );
+    }
+
+
+    protected String getInitialLabel() {
+        return "Configure";
+    }
+
+
+    protected T getCurrentValue() {
+        return currentValue;
+    }
+
+
+    protected void setCurrentValue( T value ) {
+        this.currentValue = value;
+    }
+
+
+    @Override
+    public IFormField setEnabled( boolean enabled ) {
+        if (button != null) {
+            button.setEnabled( enabled );
+            styleButton();
+        }
+        else {
+            deferredEnabled = enabled;
+        }
+        return this;
+    }
+
+
+    private void styleButton() {
+        if (!button.isEnabled()) {
+            button.setText( getDisabledButtonText( button ) );
+//            button.setBackground( getDisabledButtonBackground( button ) );
+        }
+        else {
+            button.setText( getButtonText( button ) );
+//            button.setBackground( getButtonBackground( button ) );
+        }
+    }
+
+
+    protected String getButtonText( Button button ) {
+        return "";
+    }
+
+
+    protected String getDisabledButtonText( Button button ) {
+        return "";
+    }
+
+
+    protected Color getButtonBackground( Button button ) {
+        return null;
+    }
+
+
+    protected Color getDisabledButtonBackground( Button button ) {
+        return null;
+    }
+
+
+    @Override
+    public void store() throws Exception {
+        site.setFieldValue( currentValue );
+    }
+
+
+    @Override
+    public void load() throws Exception {
+        assert button != null : "Control is null, call createControl() first.";
+
+        loadedValue = site.getFieldValue();
+
+        processLoadedValue( loadedValue );
+
+        updateButton();
+    }
+
+
+    protected abstract void processLoadedValue( Object loadedValue );
+
+
+    protected void updateButton() {
+        if (button != null) {
+            styleButton();
+        }
+    }
+}
--- plugins/org.polymap.rhei.form/src/org/polymap/rhei/field/ColorFormField.java
+++ plugins/org.polymap.rhei.form/src/org/polymap/rhei/field/ColorFormField.java
@@ -14,164 +14,60 @@
  */
 package org.polymap.rhei.field;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
-import org.polymap.rhei.form.IFormToolkit;
 
 /**
  * @author Joerg Reichert <joerg@mapzone.io>
  *
  */
 public class ColorFormField
-        implements IFormField {
+        extends AbstractDelegatingFormField<RGB> {
 
-    /**
-     * 
-     */
-    private static final String INITIAL_LABEL   = "Choose...";
-
-    private IFormFieldSite      site;
-
-    private Button              button;
-
-    private RGB                 rgb;
-
-    private Object              loadedValue;
-
-    private boolean             deferredEnabled = true;
-
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.polymap.rhei.field.IFormField#init(org.polymap.rhei.field.IFormFieldSite)
-     */
     @Override
-    public void init( IFormFieldSite site ) {
-        this.site = site;
-
+    protected String getInitialLabel() {
+        return "Choose...";
+    }
+    
+    @Override
+    protected String getButtonText( Button button ) {
+        return "";
     }
 
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.polymap.rhei.field.IFormField#dispose()
-     */
     @Override
-    public void dispose() {
-        button.dispose();
+    protected String getDisabledButtonText( Button button ) {
+        return getInitialLabel();
     }
 
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.polymap.rhei.field.IFormField#createControl(org.eclipse.swt.widgets.Composite
-     * , org.polymap.rhei.form.IFormToolkit)
-     */
     @Override
-    public Control createControl( Composite parent, IFormToolkit toolkit ) {
-        button = toolkit.createButton( parent, INITIAL_LABEL, SWT.PUSH );
-        button.addSelectionListener( new SelectionAdapter() {
-
-            public void widgetSelected( org.eclipse.swt.events.SelectionEvent e ) {
-                site.fireEvent( ColorFormField.this, IFormFieldListener.VALUE_CHANGE, rgb );
-            }
-        } );
-        button.setEnabled( deferredEnabled );
-        return button;
-    }
-
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.polymap.rhei.field.IFormField#setEnabled(boolean)
-     */
-    @Override
-    public IFormField setEnabled( boolean enabled ) {
-        if (button != null) {
-            if (!enabled) {
-                button.setBackground( null );
-            }
-            else {
-                if (rgb != null) {
-                    button.setBackground( new Color( Display.getDefault(), rgb ) );
-                }
-                else {
-                    button.setBackground( null );
-                }
-            }
-            button.setEnabled( enabled );
+    protected Color getButtonBackground( Button button ) {
+        Color color = null;
+        if(getCurrentValue() != null) {
+            color = new Color( Display.getDefault(), getCurrentValue() );
         }
-        else {
-            deferredEnabled = enabled;
-        }
-        return this;
+        return color;
     }
 
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.polymap.rhei.field.IFormField#store()
-     */
     @Override
-    public void store() throws Exception {
-        site.setFieldValue( rgb );
+    protected Color getDisabledButtonBackground( Button button ) {
+        return null;
     }
+    
 
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.polymap.rhei.field.IFormField#load()
-     */
     @Override
-    public void load() throws Exception {
-        assert button != null : "Control is null, call createControl() first.";
-
-        loadedValue = site.getFieldValue();
-
-        rgb = loadedValue instanceof RGB ? (RGB)loadedValue : null;
-
-        updateButton();
+    protected void processLoadedValue( Object loadedValue ) {
+        setCurrentValue( loadedValue instanceof RGB ? (RGB)loadedValue : null );
     }
 
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.polymap.rhei.field.IFormField#setValue(java.lang.Object)
-     */
     @Override
     public IFormField setValue( Object value ) {
         if (value instanceof RGB) {
-            rgb = (RGB)value;
+            setCurrentValue( (RGB)value );
             updateButton();
         }
         return this;
-    }
-
-
-    private void updateButton() {
-        if (button != null) {
-            if (rgb != null) {
-                button.setBackground( new Color( Display.getDefault(), rgb ) );
-            }
-            else {
-                button.setText( INITIAL_LABEL );
-                button.setBackground( null );
-            }
-        }
     }
 }
